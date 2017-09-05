f52ea2809354f56d5e6ec0f2f0e108e9353577ae
Abhishek Khetani <abhishekkhetani@live.com>
2017-09-05 18:59:55 +0530

    New npm Packages.
    New npm packages added for API call and loading button.

--- React-Native App/MyCamera/index.android.js
+++ React-Native App/MyCamera/index.android.js
@@ -13,8 +13,20 @@ import {
   Dimensions
 } from 'react-native';
 import Camera from 'react-native-camera';
+import Button from 'react-native-smart-button';
+import Toast from 'react-native-smart-loading-spinner-overlay';
+import LoadingSpinnerOverlay from 'react-native-smart-loading-spinner-overlay';
+
+//import request from 'superagent';
+import 'whatwg-fetch';
 
 export default class MyCamera extends Component {
+  constructor(){
+    super()
+    this.state = {
+      data: []
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -28,25 +40,93 @@ export default class MyCamera extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
-		 <Camera
+        <Text style={styles.instructions}>
+          {this.state.data}
+        </Text>
+		  <Camera
            ref={(cam) => {
              this.camera = cam;
            }}
            style={styles.preview}
            aspect={Camera.constants.Aspect.fill}>
-           <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+           <Button
+                    onPress={this.takePicture.bind(this)}
+                    touchableType={Button.constants.touchableTypes.fadeContent}
+                    style={{margin: 10, height: 40, backgroundColor: 'white', borderRadius: 3, borderWidth: StyleSheet.hairlineWidth, borderColor: 'white', alignSelf: 'stretch'}}
+                    textStyle={{fontSize: 17, color: 'gray'}}>
+                      Know me!
+          </Button>
+
        </Camera>
+       <LoadingSpinnerOverlay ref={ component => this._modalLoadingSpinnerOverLay = component }/>
       </View>
-	 
+
     );
   }
-  
-takePicture() {
-   this.camera.capture()
-     .then((data) => console.log(data))
-     .catch(err => console.error(err));
+
+
+  takePicture(e) {
+    e.preventDefault();
+     this._modalLoadingSpinnerOverLay.show();
+     this.camera.capture()
+       .then((data) => this._showModalLoadingSpinnerOverLay(data))
+       .catch(err => console.error(err));
  }
- 
+
+ _showModalLoadingSpinnerOverLay = (data) => {
+
+    function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+          return response;
+      } else {
+          var error = new Error(response.statusText);
+          error.response = response;
+          throw error;
+        }
+    }
+
+    function parseJSON(response) {
+        return response.json();
+    }
+
+    var imageUri = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/PerfectStrawberry.jpg/220px-PerfectStrawberry.jpg';
+    var source = {
+      imageUri : imageUri
+    };
+    var image = {
+      source : source
+    };
+    var type = 'LABEL_DETECTION';
+    var featuresElement = {
+      type : type
+    };
+    var features = [featuresElement];
+    var requestsElement = {
+      image : image,
+      features : features
+    };
+    var requests = [requestsElement];
+
+    fetch('https://vision.googleapis.com/v1/images:annotate', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 'request' : requests })
+    })
+      //.then(checkStatus)
+      .then(function(response) {alert(response);})
+      .catch(function(error) {
+          console.log('request failed', error);
+          alert(error.error.code);
+    });
+
+    setTimeout(
+      () => { this._modalLoadingSpinnerOverLay.hide();
+    }, 2000 );
+
+  }
 }
 
 const styles = StyleSheet.create({
